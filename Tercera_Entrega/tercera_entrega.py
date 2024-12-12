ba592a07abf6ff864534f39b806d7fe79de08994
MatWo1996 <matthias.wohlmacher@realcore.de>
2024-12-12 12:11:31 +0100

    First files created

---
+++ Tercera_Entrega/tercera_entrega.py
@@ -0,0 +1,10 @@
+import weka.core.jvm as jwm
+from weka.core.converters import Loader
+from weka.classifiers import Classifier, Evaluation
+from weka.filters import Filter
+from weka.core.classes import Random
+
+jwm.start()
+## El código empieza aquí.
+
+jwm.stop()
